e9048003c7988c58cedda5287bfa81cdb698dc35
fakynno <fakynno@gmail.com>
2022-07-17 22:03:54 -0300

    alterado o lugar da variavel

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-let deveImprimir = true;
 function somaNumero(numero1, numero2) {
     return numero1 + numero2;
 }
+let deveImprimir = true;
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log('Resultado da soma: ', somaNumero(Number(input1.value), Number(input2.value)));
+            console.log('Resultado da soma: ', somaNumero(Number(input1.value), Number(input2.value), deveImprimir));
         }
     });
 }
--- app.ts
+++ app.ts
@@ -2,16 +2,17 @@ let button = document.getElementById('button');
 let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2') as HTMLInputElement;
 
-let deveImprimir = true;
 
 function somaNumero(numero1: number, numero2: number) {
     return numero1 + numero2;
 }
 
+let deveImprimir = true;
+
 if(button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log('Resultado da soma: ', somaNumero(Number(input1.value), Number(input2.value)));
+            console.log('Resultado da soma: ', somaNumero(Number(input1.value), Number(input2.value), deveImprimir));
         }
     })
 }
