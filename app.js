52966a477de35e5b5067f4eea1f7aee9176ee38b
fakynno <fakynno@gmail.com>
2022-07-17 22:04:30 -0300

    add parametro na funcao

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-function somaNumero(numero1, numero2) {
+function somaNumero(numero1, numero2, deveImprimir) {
     return numero1 + numero2;
 }
 let deveImprimir = true;
--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2') as HTMLInputElement;
 
 
-function somaNumero(numero1: number, numero2: number) {
+function somaNumero(numero1: number, numero2: number, deveImprimir: boolean) {
     return numero1 + numero2;
 }
 
