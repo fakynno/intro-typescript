ec08bae6d3be8214b738db78735a1f9c1adf5e33
fakynno <fakynno@gmail.com>
2022-07-17 21:53:52 -0300

    add log para pegar resposta

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function somaNumero(numero1, numero2) {
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            somaNumero(Number(input1.value), Number(input2.value));
+            console.log('Resultado da soma: ', somaNumero(Number(input1.value), Number(input2.value)));
         }
     });
 }
