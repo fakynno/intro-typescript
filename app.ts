3e745a3db3e91b28c4d933f3c0acde43427f9814
fakynno <fakynno@gmail.com>
2022-07-17 21:48:41 -0300

    Add funcao para somar valores

--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
 let button = document.getElementById('button');
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+function somaNumero(numero1: number, numero2: number) {
+    return numero1 + numero2;
+}
+
+if(button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            somaNumero(Number(input1.value), Number(input2.value));
+        }
+    })
+}
