59bdef7ff80cf04f26821a8598b4e097b1491520
fakynno <fakynno@gmail.com>
2022-07-19 06:49:59 -0300

    arq renomeado e movido para outra pasta

---
+++ exemplos/tipo-any.js
@@ -0,0 +1,13 @@
+"use strict";
+let valorAny;
+valorAny = 3;
+valorAny = 'ola';
+valorAny = true;
+let valorString = 'teste';
+valorString = valorAny;
+let valorString2 = 'outro teste';
+valorString2 = valorAny;
+function somaString(string1, string2) {
+    console.log(string1 + string2);
+}
+somaString('ola ', 'como vai?');
---
+++ exemplos/tipo-any.ts
@@ -0,0 +1,15 @@
+let valorAny: any;
+valorAny = 3;
+valorAny = 'ola';
+valorAny = true;
+
+let valorString: string = 'teste';
+valorString = valorAny;
+let valorString2: string = 'outro teste';
+valorString2 = valorAny;
+
+function somaString(string1: string, string2: string) {
+    console.log(string1 + string2);    
+}
+
+somaString('ola ', 'como vai?');
