f1cfd22c4541fcec898b6e575a2d51924e7bd660
fakynno <fakynno@gmail.com>
2022-07-19 07:02:24 -0300

    Exemplo com type

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ function somarValores(entrada1, entrada2) {
 somarValores(10, 12);
 somarValores('meu nome é ', 'fabio');
 somarValores('que horas são? ', 7);
+function soma(valor1, valor2) {
+    if (typeof valor1 === 'string' || typeof valor2 === 'string') {
+        return console.log(valor1.toString() + valor2.toString());
+    }
+    else {
+        return console.log(valor1 - valor2);
+    }
+}
+soma(100, 13);
+soma('eu sou ', 'voce');
+soma('Oi ', 80);
--- app.ts
+++ app.ts
@@ -11,3 +11,16 @@ function somarValores(entrada1: number | string, entrada2: number | string) {
 somarValores(10,12);
 somarValores('meu nome é ', 'fabio');
 somarValores('que horas são? ', 7);
+
+
+function soma(valor1: entrada, valor2: entrada) {
+    if (typeof valor1 === 'string' || typeof valor2 === 'string') {
+        return console.log(valor1.toString() + valor2.toString());
+    } else {
+        return console.log(valor1 - valor2);
+    }
+}
+
+soma(100,13);
+soma('eu sou ','voce');
+soma('Oi ',80);
