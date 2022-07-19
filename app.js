544284af04f1840c202ed691caaa2b0aa7b1542f
fakynno <fakynno@gmail.com>
2022-07-19 06:49:26 -0300

    removido o arquivo app

--- app.js
+++ app.js
@@ -1,47 +1,13 @@
 "use strict";
-const pessoa = {
-    nome: 'Fabio',
-    idade: 39,
-    profissao: 'desenvolvedor'
-};
-pessoa.idade = 33;
-const novaPessoa = {
-    nome: 'fabricio',
-    idade: 12,
-    profissao: 'pedreiro'
-};
-const outraPessoa = {
-    nome: 'Mauricio',
-    idade: 26,
-    profissao: 'Desenvolvedor'
-};
-var Profissao;
-(function (Profissao) {
-    Profissao[Profissao["Desenvolvedor"] = 0] = "Desenvolvedor";
-    Profissao[Profissao["Atriz"] = 1] = "Atriz";
-    Profissao[Profissao["Professor"] = 2] = "Professor";
-    Profissao[Profissao["Pedreiro"] = 3] = "Pedreiro";
-})(Profissao || (Profissao = {}));
-const pessoaInterface = {
-    nome: 'Jose',
-    idade: 35,
-    profissao: Profissao.Pedreiro
-};
-const novoEstudante = {
-    nome: 'Luigi',
-    idade: 13,
-    profissao: Profissao.Professor,
-    materias: ['Português', 'Inglês', 'Programação', 'Ciências', 'História']
-};
-const outroEstudante = {
-    nome: 'Gabriel',
-    idade: 16,
-    materias: ['Inglês', 'Português']
-};
-console.log('lista de matérias: ');
-function listar(lista) {
-    for (let item of lista) {
-        console.log(' - ', item);
-    }
+let valorAny;
+valorAny = 3;
+valorAny = 'ola';
+valorAny = true;
+let valorString = 'teste';
+valorString = valorAny;
+let valorString2 = 'outro teste';
+valorString2 = valorAny;
+function somaString(string1, string2) {
+    console.log(string1 + string2);
 }
-listar(novoEstudante.materias);
+somaString('ola ', 'como vai?');
