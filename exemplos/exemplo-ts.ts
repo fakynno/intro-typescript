1296ae1bcc37a49f414574e8c7d19a959cee4924
fakynno <fakynno@gmail.com>
2022-07-19 06:38:24 -0300

    Testando validacao de variaveis

---
+++ exemplos/exemplo-ts.ts
@@ -0,0 +1,23 @@
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+
+function somaNumero(numero1: number, numero2: number, deveImprimir: boolean, frase: string) {
+    let soma = numero1 + numero2;
+    if (deveImprimir) {
+        console.log(frase + soma);        
+    }
+    return soma;
+}
+
+let deveImprimir = true;
+let frase = 'O resultado é: ';
+
+if(button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(somaNumero(Number(input1.value), Number(input2.value), deveImprimir, frase));
+        }
+    })
+}
