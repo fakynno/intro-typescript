94f95ce8592aacff27eaad476fe063c883bddf94
fakynno <fakynno@gmail.com>
2022-07-20 21:53:01 -0300

    resolucao do exercicio

--- exemplos/desafios/desafio01/desafio01.ts
+++ exemplos/desafios/desafio01/desafio01.ts
@@ -1,3 +1,37 @@
-let empregado = {}
-empregado.code = 10;
-empregado.name = "Fabio";
+/* forma 01 */
+
+const empregado01 = {
+    code: 10,
+    name: "Fabio"
+}
+
+// o let aqui não precisa ter valores inicializados
+let empregado03: {
+    nome: string,
+    codigo: number;
+}
+
+/* forma 02 */
+
+const empregado02: { nome: string, codigo: number} = {
+    nome: "fabio",
+    codigo: 15
+}
+
+
+/* forma 03 */
+
+interface Empregado {
+    nomeFuncionario: string,
+    codigoFuncionario: number
+};
+
+
+const objFuncionario1 = {} as Empregado;
+objFuncionario1.codigoFuncionario = 10;
+objFuncionario1.nomeFuncionario = "marcelo";
+
+const objFuncionario2: Empregado = {
+    nomeFuncionario: 'fabio',
+    codigoFuncionario: 12
+}
